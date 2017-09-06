3c14079d99274a08ebfe8b3625847dd7e821daa0
Nazim Aliyev <aliyevn@amazon.com>
2017-09-06 15:19:05 +0000

    Adding lambda function to treasure box

---
+++ lambda_ddbt_sync.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import boto3
+import json
+
+DDBT_REPLICA = 'moviedata-replica'
+client = boto3.client('dynamodb', region_name = 'us-east-2')
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT' or record['eventName'] == 'MODIFY':
+            client.put_item(TableName = DDBT_REPLICA, Item = record['dynamodb']['NewImage'])
+        elif record['eventName'] == 'REMOVE':
+            client.delete_item(TableName = DDBT_REPLICA, Key = record['dynamodb']['Keys'])
+
+    return 'Successfully processed {} records.'.format(len(event['Records']))
